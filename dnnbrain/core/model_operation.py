66c0a7179035f57aab522bb7d0eaa19edc54ddaf
Yukun Qu <532021098@qq.com>
2019-07-16 11:12:58 +0800

    "1.add function to extract activaton of fc layer  2.add funcation to reconstruct fc layer from 1000 classifier to n classifier"

--- dnnbrain/core/model_operation.py
+++ dnnbrain/core/model_operation.py
@@ -28,21 +28,47 @@ def dnn_activation(input, net, layer, channel=None):
     dnnact[numpy.array]: DNN activation, A 4D dataset with its format as pic*channel*unit*unit
     """
     loader = iofiles.NetLoader(net)
-    actmodel = truncate_net(loader.model, loader.layer2indices[layer])
+    if 'conv' in layer:
+        dnnact =  dnn_activaiton_conv(input,loader,layer)
+    elif 'fc' in layer:
+        dnnact = dnn_activation_fc(input,loader,layer)
+    else:
+        raise Exception("Not support this layer,please entry conv or fc")
+    if channel:
+        channel_new = [cl - 1 for cl in channel]
+        dnnact = dnnact[:, channel_new, :, :]
+    return dnnact
+
+
+def dnn_activaiton_conv(input,netloader,layer):
+    actmodel = truncate_net_conv(netloader.model, netloader.layer2indices[layer])
     dnnact = []
     for _, picdata, target in input:
         dnnact_part = actmodel(picdata)
         dnnact.extend(dnnact_part.detach().numpy())
     dnnact = np.array(dnnact)
-    if channel:
-        channel_new = [cl-1 for cl in channel]
-        dnnact = dnnact[:, tuple(channel_new), :, :]
     return dnnact
 
 
-def truncate_net(net, indices):
+def dnn_activation_fc(input,netloader,layer):
+    actmodel = netloader.model
+    indices = netloader.layer2indices[layer]
+    new_classifier = nn.Sequential(*list(netloader.model.children())[-1][:indices[1] + 1])
+    if hasattr(actmodel,'classifier'):
+        actmodel.classifier = new_classifier
+    elif hasattr(actmodel,'fc'):
+        actmodel.fc = new_classifier
+    dnnact = []
+    for _, picdata, target in input:
+        dnnact_part = actmodel(picdata)
+        dnnact.extend(dnnact_part.detach().numpy())
+    dnnact = np.array(dnnact)
+    return dnnact
+
+
+def truncate_net_conv(net, indices):
     """
-    truncate the neural network at the specified layer number
+    truncate the neural network at the specified convolution layer
 
     Parameters:
     -----------
@@ -54,10 +80,25 @@ def truncate_net(net, indices):
     truncated_net[torch.nn.Sequential]
     """
     if len(indices) > 1:
-        tmp = list(net.children())[:indices[0]]
+        tmp  = list(net.children())[:indices[0]]
         next = list(net.children())[indices[0]]
-        return nn.Sequential(*(tmp+[truncate_net(next, indices[1:])]))
+        return nn.Sequential(*(tmp+[truncate_net_conv(next, indices[1:])]))
     elif len(indices) == 1:
         return nn.Sequential(*list(net.children())[:indices[0]+1])
     else:
         raise ValueError("Layer indices must not be empty!")
+
+
+def dnn_finetuning(netloader,layer,out_features):
+    """can reconstruct from any layer"""
+    dnn_model = netloader.model
+    indices = netloader.layer2indices[layer]
+    old_classifier = list(netloader.model.children())[-1][:indices[1]]
+    in_features = list(netloader.model.children())[-1][indices[1]].in_features
+    new_classifier =   nn.Sequential(*(old_classifier + list(nn.Linear(in_features,out_features))))
+
+    if hasattr(dnn_model,'classifier'):
+        dnn_model.classifier = new_classifier
+    elif hasattr(dnn_model,'fc'):
+        dnn_model.fc = new_classifier
+    return dnn_model
