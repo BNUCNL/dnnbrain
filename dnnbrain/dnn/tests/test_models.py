5508470e58b4ef1e796578425d1a19aaf5578d9c
Taicheng Huang <taicheng_huang@sina.cn>
2019-08-04 19:10:02 +0800

    write test script to test models, while code hasn't finished yet

---
+++ dnnbrain/dnn/tests/test_models.py
@@ -0,0 +1,26 @@
+import pytest
+from dnnbrain.dnn import io as iofiles
+from dnnbrain.dnn import models
+
+@pytest.mark.skip
+def test_dnn_truncate():
+    """
+    Test dnn_truncate
+    """
+    pass
+    
+
+@pytest.mark.skip
+def test_dnn_train_model():
+    """
+    Test dnn_train_model
+    """
+    pass
+    
+    
+@pytest.mark.skip
+def test_dnn_test_model():
+    """
+    Test dnn_test_model
+    """
+    pass
