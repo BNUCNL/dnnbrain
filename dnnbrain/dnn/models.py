a513f6d5b48a11aee56a2474b30b9587648819f7
Taicheng Huang <taicheng_huang@sina.cn>
2019-08-07 15:05:11 +0800

    1 modified dnn_truncate in case of error when layer contains fc. 2 rewrite TransferredNet as GraftLayer, and designed new interface for it

--- dnnbrain/dnn/models.py
+++ dnnbrain/dnn/models.py
@@ -24,6 +24,14 @@ def dnn_truncate(netloader, layer):
     indices = netloader.layer2indices[layer]
     model_frame = nn.Sequential(*netloader.model.children())
     truncate_model = _get_truncate_layers(model_frame, indices)
+    if 'fc' in layer:
+        # Re-define forward method
+        def forward(x):
+            x = truncate_model[:-1](x)
+            x = torch.flatten(x, 1)
+            x = truncate_model[-1](x)
+            return x
+        truncate_model.forward = forward
     return truncate_model
 
 
@@ -135,6 +143,50 @@ def dnn_test_model(dataloaders, model):
     return model_target, actual_target, test_acc
 
 
+class GraftLayer(nn.Module):
+    def __init__(self, netloader, layer, fc_out_num, channel=None, feature_extract=True):
+        """
+        Connect the truncated_net to a full connection layer.
+
+        Parameters:
+        -----------
+        truncated_net[torch.nn.Module]: a truncated neural network from the pretrained network
+        fc_in_num[int]: the number of the in_features of the full connection layer
+        fc_out_num[int]: the number of the out_features of the full connection layer
+        channel[iterator]: The indices of out_channels of the selected convolution layer
+        feature_extract[bool]: If feature_extract = False, the model is finetuned and all model parameters are updated.
+            If feature_extract = True, only the last layer parameters are updated, the others remain fixed.
+            https://pytorch.org/tutorials/beginner/finetuning_torchvision_models_tutorial.html
+        """
+        super(GraftLayer, self).__init__()
+        self.truncate_net = dnn_truncate(netloader, layer)
+        if feature_extract:
+            for param in self.truncate_net.parameters():
+                param.requires_grad = False
+
+        # Set a test data to get output size of the truncate_net
+        indicator_val = torch.randn((1,3,netloader.img_size[0],netloader.img_size[1]))
+        indicator_output = self.truncate_net(indicator_val)
+        
+        if channel is not None:
+            assert 'conv' in layer, "Selected channel only happened in convolution layer."
+        self.channel = channel
+        # fc input number
+        fc_in_num = indicator_output.view(indicator_output.size(0),-1).shape[-1]
+        self.fc = nn.Linear(fc_in_num, fc_out_num)
+    
+    def forward(self, x):
+        x = self.truncate_net(x)
+        # if self.channel is not None :
+        # I'm not quite sure how to trained a model with some channels but not all.
+        # Therefore channel is not be finished.
+        # The original one is
+        # if self.channel is not None:
+        #   x = x[:, self.channel]
+        x = x.view(x.size(0), -1)
+        x = self.fc(x)
+        return x
+
 
 class TransferredNet(nn.Module):
     def __init__(self, truncated_net, fc_in_num, fc_out_num, channel=None, feature_extract=True):
@@ -167,7 +219,7 @@ class TransferredNet(nn.Module):
         x = x.view(x.size(0), -1)  # (batch_num, unit_num)
         x = self.fc(x)
         return x
-
+        
 
 class Vgg_face(nn.Module):
     """Vgg_face's model architecture"""
