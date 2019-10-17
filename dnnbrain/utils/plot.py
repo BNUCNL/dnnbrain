727a930c341729c3bdeadd062608b60547a09bbc
Xiayu Chen <954830460@qq.com>
2019-10-17 16:57:49 +0800

    RF: tidy annotations

--- dnnbrain/utils/plot.py
+++ dnnbrain/utils/plot.py
@@ -1,16 +1,9 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Oct 17 14:39:00 2019
-
-@author: liuxingyu
-"""
-
 import matplotlib.pyplot as plt
 import numpy as np
 
 def imgarray_show(x, nrows=1, ncols=1, row_label=None, vmin=None, vmax=None,
-               figsize=[10, 6], cmap='coolwarm', frame_on=True,
-               show=True, save_path=None):
+                  figsize=[10, 6], cmap='coolwarm', frame_on=True,
+                  show=True, save_path=None):
     """
     create a figure showing multiple images.
     
@@ -18,16 +11,16 @@ def imgarray_show(x, nrows=1, ncols=1, row_label=None, vmin=None, vmax=None,
     ----------
     x[list]: list of image array (2d or 3d-RGB[A])
     nrows, ncols[int]: number of rows/columns of the subplot grid
-    row_label[list]: lsit of str 
-    vmin, vmax[scalar]: vmin and vmax define the data range 
-        that the colormap covers. By default, the colormap covers 
-        the complete value range of the supplied data. 
+    row_label[list]: row names
+    vmin, vmax[scalar]: vmin and vmax define the value range of 
+        colormap applied to all images. By default, colormaps
+        adapt to each image's value range.
     figsize[float, float]: width, height of figure in inches.
     cmap[str]: The Colormap instance or registered colormap name used 
         to map scalar data to colors. 
     frame_on[bool]: set whether the axes rectangle patch is drawn
     show[bool]: set whether the figure is displayed
-    
+    save_path[str]: file path to save the figure
     """
 
     fig, axs = plt.subplots(nrows=nrows, ncols=ncols,
