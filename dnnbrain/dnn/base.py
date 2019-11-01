06b9f7a8ca16b0f8d173fc0e9c4883cc19f8e617
sunshineDrizzle <954830460@qq.com>
2019-11-01 16:16:57 +0800

    NF: ImageSet and VideoSet

---
+++ dnnbrain/dnn/base.py
@@ -0,0 +1,131 @@
+import cv2
+import torch
+import numpy as np
+
+from PIL import Image
+from os.path import join as pjoin
+from torchvision import transforms
+
+
+class ImageSet:
+    """
+    Build a dataset to load image
+    """
+    def __init__(self, img_dir, img_ids, labels=None, transform=None):
+        """
+        Initialize ImageSet
+
+        Parameters:
+        ----------
+        img_dir[str]: images' parent directory
+        img_ids[list]: Each img_id is a path which can find the image file relative to img_dir.
+        labels[list]: Each image's label.
+        transform[callable function]: optional transform to be applied on a stimulus.
+        """
+        self.img_dir = img_dir
+        self.img_ids = img_ids
+        self.labels = np.ones(len(self.img_ids)) if labels is None else labels
+        self.transform = transforms.Compose([transforms.ToTensor()]) if transform is None else transform
+
+    def __len__(self):
+        """
+        Return the number of images
+        """
+        return len(self.img_ids)
+
+    def __getitem__(self, indices):
+        """
+        Get image data and corresponding labels
+
+        Parameter:
+        ---------
+        indices[int|list|slice]: subscript indices
+
+        Returns:
+        -------
+        data[tensor]: image data with shape as (n_stim, n_chn, height, weight)
+        labels[list]: image labels
+        """
+        if isinstance(indices, int):
+            tmp_ids = [self.img_ids[indices]]
+            labels = [self.labels[indices]]
+        elif isinstance(indices, list):
+            tmp_ids = [self.img_ids[idx] for idx in indices]
+            labels = [self.labels[idx] for idx in indices]
+        elif isinstance(indices, slice):
+            tmp_ids = self.img_ids[indices]
+            labels = self.labels[indices]
+        else:
+            raise IndexError("only integer, slices (`:`) and list are valid indices")
+
+        data = torch.zeros(0)
+        for img_id in tmp_ids:
+            image = Image.open(pjoin(self.img_dir, img_id))  # load image
+            image = self.transform(image)  # transform image
+            image = torch.unsqueeze(image, 0)
+            data = torch.cat((data, image))
+
+        return data, labels
+
+
+class VideoSet:
+    """
+    Dataset for video data
+    """
+    def __init__(self, vid_file, frame_nums, labels=None, transform=None):
+        """
+        Parameters:
+        ----------
+        vid_file[str]: video data file
+        frame_nums[list]: sequence numbers of the frames of interest
+        labels[list]: each frame's label
+        transform[pytorch transform]
+        """
+        self.vid_cap = cv2.VideoCapture(vid_file)
+        self.frame_nums = frame_nums
+        self.labels = np.ones(len(self.frame_nums)) if labels is None else labels
+        self.transform = transforms.Compose([transforms.ToTensor()]) if transform is None else transform
+
+    def __getitem__(self, indices):
+        """
+        Get frame data and corresponding labels
+
+        Parameter:
+        ---------
+        indices[int|list|slice]: subscript indices
+
+        Returns:
+        -------
+        data[tensor]: frame data with shape as (n_stim, n_chn, height, weight)
+        labels[list]: frame labels
+        """
+        if isinstance(indices, int):
+            tmp_nums = [self.frame_nums[indices]]
+            labels = [self.labels[indices]]
+        elif isinstance(indices, list):
+            tmp_nums = [self.frame_nums[idx] for idx in indices]
+            labels = [self.labels[idx] for idx in indices]
+        elif isinstance(indices, slice):
+            tmp_nums = self.frame_nums[indices]
+            labels = self.labels[indices]
+        else:
+            raise IndexError("only integer, slices (`:`) and list are valid indices")
+
+        data = torch.zeros(0)
+        for frame_num in tmp_nums:
+            # get frame
+            self.vid_cap.set(cv2.CAP_PROP_POS_FRAMES, frame_num-1)
+            _, frame = self.vid_cap.read()
+            frame = Image.fromarray(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
+
+            frame = self.transform(frame)  # transform frame
+            frame = torch.unsqueeze(frame, 0)
+            data = torch.cat((data, frame))
+
+        return data, labels
+
+    def __len__(self):
+        """
+        Return the number of frames
+        """
+        return len(self.frame_nums)
