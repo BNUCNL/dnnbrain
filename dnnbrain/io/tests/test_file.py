224e2262a13a1ba2c3ee33f2c63537ea42ea9614
zpp <1293456247@qq.com>
2019-11-02 07:21:17 -0400

    Activitation_test_write

--- dnnbrain/io/tests/test_file.py
+++ dnnbrain/io/tests/test_file.py
@@ -1,7 +1,15 @@
 import os
 import unittest
+import numpy as np
+import h5py
 
+from dnnbrain.io.file import ActivationFile
 from os.path import join as pjoin
+from torch.utils.data import DataLoader
+from torchvision.transforms import Compose, Resize, ToTensor
+from dnnbrain.dnn.core import DNN, Stimulus, Mask
+from dnnbrain.dnn.base import ImageSet
+
 
 DNNBRAIN_TEST = pjoin(os.environ['DNNBRAIN_DATA'], 'test')
 TMP_DIR = pjoin(os.path.expanduser('~'), '.dnnbrain_tmp')
@@ -23,9 +31,25 @@ class TestActivationFile(unittest.TestCase):
     def test_read(self):
         pass
 
-    def test_write(self):
-        pass
-
+    def test_write(self):        
+        fpath = pjoin(TMP_DIR,'test.act.h5')
+        act = {}
+        layers = list(range(10))
+        for i in range(10):
+            layers[i] = f"layers{i}"
+        for i in range(10):
+            act.update({layers[i]: {'data':np.random.randn(2,3),'raw_shape':(2,3)}})
+        
+        # write
+        ActivationFile(fpath).write(act)
+        
+        
+        # compare
+        rf = h5py.File(fpath,'r')
+        for i in range(10):
+            self.assertTrue(np.all(act[layers[i]]['data'] == np.array(rf[layers[i]])))
+            self.assertTrue(np.all(np.array(act[layers[i]]['raw_shape']) == rf[layers[i]].attrs['raw_shape']))
+            
 
 class TestMaskFile(unittest.TestCase):
 
@@ -34,3 +58,6 @@ class TestMaskFile(unittest.TestCase):
 
     def test_write(self):
         pass
+
+if __name__=='__main__':
+    unittest.main()
