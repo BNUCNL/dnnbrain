b720348dd48dfc33343b2316592025016033c80e
XU Wei <psyxw@outlook.com>
2019-11-02 18:08:00 +0800

    Update test_base.py - TestVideoSet

--- dnnbrain/dnn/tests/test_base.py
+++ dnnbrain/dnn/tests/test_base.py
@@ -1,6 +1,10 @@
 import os
+import cv2
 import unittest
-
+import torch
+from PIL import Image
+from dnn.base import VideoSet
+from torchvision import transforms
 from os.path import join as pjoin
 
 DNNBRAIN_TEST = pjoin(os.environ['DNNBRAIN_DATA'], 'test')
@@ -24,4 +28,18 @@ class TestVideoSet(unittest.TestCase):
         pass
 
     def test_getitem(self):
-        pass
+        vid_file = pjoin(DNNBRAIN_TEST, 'video', \
+                         'sub-CSI1_ses-01_imagenet.mp4')
+        frame_nums = [12,13,15,16]
+        dataset = VideoSet(vid_file, frame_nums)
+        transform = transforms.Compose([transforms.ToTensor()])
+        
+        for i in list(range(len(frame_nums))):
+            tmp_video1, _ = dataset[i]
+            cap = cv2.VideoCapture(vid_file)
+            for j in range(frame_nums[i]):
+                _, tmp_video2 = cap.read()
+            frame = Image.fromarray(cv2.cvtColor(tmp_video2, cv2.COLOR_BGR2RGB))
+            tmp_video2 = transform(frame)
+            ist = torch.equal(tmp_video1,tmp_video2)
+            print(ist)
