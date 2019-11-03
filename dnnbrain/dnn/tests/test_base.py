b3f20646a71be98798e4f39df0d10cb9aa4a15fb
Blink <1458940886@qq.com>
2019-11-02 22:30:55 -0400

    change TestImageSet

--- dnnbrain/dnn/tests/test_base.py
+++ dnnbrain/dnn/tests/test_base.py
@@ -19,23 +19,25 @@ class TestImageSet(unittest.TestCase):
 
     def test_init(self):
 
-        #test when labels and transform are None
+    #test when labels and transform are None
         img_dir = pjoin(DNNBRAIN_TEST, 'image', 'images')
         img_ids = ['n01443537_2819.JPEG', 'n01531178_2651.JPEG']
 
         dataset = ImageSet(img_dir,img_ids)
 
+        #test dir ids labels
         self.assertEqual(dataset.img_dir,'/nfs/s2/dnnbrain_data/test/image/images')
         self.assertEqual(dataset.img_ids, ['n01443537_2819.JPEG', 'n01531178_2651.JPEG'])
         self.assertTrue(np.all(dataset.labels == np.array([1,1])))
-
+        
+        #test transform
         for img_id in dataset.img_ids:
             image = Image.open(pjoin(dataset.img_dir, img_id))  # load image
-            image_a = dataset.transform(image)
-            image_b = transforms.Compose([transforms.ToTensor()])(image)
-            self.assertTrue(torch.equal(image_a,image_b))
+            image_org = dataset.transform(image)               
+            image_new = transforms.Compose([transforms.ToTensor()])(image)
+            self.assertTrue(torch.equal(image_org,image_new))   #compare original image and new image
         
-        #test when labels and transform are given
+    #test when labels and transform are given
         labels = [1, 11]
         transform = transforms.Compose([transforms.Resize((224, 224)),
                                 transforms.ToTensor()])
@@ -46,64 +48,56 @@ class TestImageSet(unittest.TestCase):
         
         for img_id in dataset.img_ids:
             image = Image.open(pjoin(dataset.img_dir, img_id))  # load image
-            image_a = dataset.transform(image)
-            image_b = transforms.Compose([transforms.Resize((224, 224)),transforms.ToTensor()])(image)
-            self.assertTrue(torch.equal(image_a,image_b))
+            image_org = dataset.transform(image)
+            image_new = transforms.Compose([transforms.Resize((224, 224)),transforms.ToTensor()])(image)
+            self.assertTrue(torch.equal(image_org,image_new))
     
     def test_getitem(self):
         
-        img_par = pjoin(DNNBRAIN_TEST, 'image', 'images')
+        #initialize the dir ids labels transform & dataset
+        img_dir = pjoin(DNNBRAIN_TEST, 'image', 'images')
         img_ids = ['n01443537_2819.JPEG', 'n01531178_2651.JPEG','n07695742_5848.JPEG','n02655020_1972.JPEG','n01641577_1229.JPEG']
-
+        labels = [1,11,32,397,30]
         transform = transforms.Compose([transforms.Resize((224, 224)),
                                         transforms.ToTensor()])
-        labels = [1,11,32,397,30]
         
-        dataset = ImageSet(img_par,img_ids,labels,transform)
+        dataset = ImageSet(img_dir,img_ids,labels,transform)
         
         #test indice int
         indices = 3
-        data,labels_get = dataset.__getitem__(indices)
+        image_org,labels_get = dataset[indices]
         
-        img_org = dataset[indices]
-        img0 = img_org[0]
-        img_0 = transform(Image.open(pjoin(img_par, img_ids[indices])))
+        image_new = transform(Image.open(pjoin(img_dir, img_ids[indices])))
 
-        self.assertTrue(np.all(img0.numpy() == img_0.numpy()))
+        self.assertTrue(torch.equal(image_org,image_new))
         self.assertEqual(labels_get[0],labels[indices])
         
         #test indice list
         indices = [1,2]
-        data,labels_get = dataset.__getitem__(indices)
-        
-        img_org = dataset[indices]
-        img0 = img_org[0]
+        image_org,labels_get = dataset[indices]
         
         tmp_ids = [dataset.img_ids[i] for i in indices]
-        img_0 = torch.zeros(0)
+        image_new = torch.zeros(0)
         for img_id in tmp_ids:
-            img_tmp = transform(Image.open(pjoin(img_par, img_id)))
+            img_tmp = transform(Image.open(pjoin(img_dir, img_id)))
             img_tmp = torch.unsqueeze(img_tmp, 0)
-            img_0 = torch.cat((img_0, img_tmp))
+            image_new = torch.cat((image_new, img_tmp))
 
-        self.assertTrue(np.all(img0.numpy() == img_0.numpy()))
+        self.assertTrue(torch.equal(image_org,image_new))
         self.assertEqual(labels_get,[labels[i] for i in indices])
         
         #test indice slice
         indices = slice(1,3)
-        data,labels_get = dataset.__getitem__(indices)
-
-        img_org = dataset[indices]  
-        img0 = img_org[0]
+        image_org,labels_get = dataset[indices]
         
         tmp_ids = dataset.img_ids[indices]
-        img_0 = torch.zeros(0)
+        image_new = torch.zeros(0)
         for img_id in tmp_ids:
-            img_tmp = transform(Image.open(pjoin(img_par, img_id)))
+            img_tmp = transform(Image.open(pjoin(img_dir, img_id)))
             img_tmp = torch.unsqueeze(img_tmp, 0)
-            img_0 = torch.cat((img_0, img_tmp))
+            image_new = torch.cat((image_new, img_tmp))
 
-        self.assertTrue(np.all(img0.numpy() == img_0.numpy()))
+        self.assertTrue(torch.equal(image_org,image_new))
         self.assertEqual(labels_get,labels[indices])               
         
 class TestVideoSet(unittest.TestCase):
