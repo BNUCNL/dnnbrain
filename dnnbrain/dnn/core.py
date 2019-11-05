e428ec3a681bda81bcff6784c660203c35978c53
sunshineDrizzle <954830460@qq.com>
2019-11-05 20:05:22 +0800

    RM: remove dnn_activation

--- dnnbrain/dnn/core.py
+++ dnnbrain/dnn/core.py
@@ -267,6 +267,7 @@ class DNN:
 
             # extract DNN activation
             for stims, _ in data_loader:
+                # stimuli with shape as (n_stim, n_chn, height, width)
                 self.model(stims)
                 print('Extracted activation of {0}: {1}/{2}'.format(
                     layer, len(acts_holder), n_stim))
@@ -719,49 +720,6 @@ class Mask:
     @property
     def layers(self):
         return list(self._dmask.keys())
-
-
-def dnn_activation(data, model, layer_loc, channels=None):
-    """
-    Extract DNN activation from the specified layer
-
-    Parameters:
-    ----------
-    data[tensor]: input stimuli of the model with shape as (n_stim, n_chn, height, width)
-    model[model]: DNN model
-    layer_loc[sequence]: a sequence of keys to find the location of
-        the target layer in the DNN model. For example, the location of the
-        fifth convolution layer in AlexNet is ('features', '10').
-    channels[list]: channel indices of interest
-
-    Return:
-    ------
-    dnn_acts[array]: DNN activation
-        a 4D array with its shape as (n_stim, n_chn, n_r, n_c)
-    """
-    # change to eval mode
-    model.eval()
-
-    # prepare dnn activation hook
-    dnn_acts = []
-
-    def hook_act(module, input, output):
-        act = output.detach().numpy().copy()
-        if channels is not None:
-            act = act[:, channels]
-        dnn_acts.append(act)
-
-    module = model
-    for k in layer_loc:
-        module = module._modules[k]
-    hook_handle = module.register_forward_hook(hook_act)
-
-    # extract dnn activation
-    model(data)
-    dnn_acts = dnn_acts[0]
-
-    hook_handle.remove()
-    return dnn_acts
 
 
 def db_uva(dnn_acts, resp, model, iter_axis=None, cvfold=3):
