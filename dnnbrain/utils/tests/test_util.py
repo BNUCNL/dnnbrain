54171625ecbfb8715a07147e41dcff6277c7e362
sunshineDrizzle <954830460@qq.com>
2019-11-13 16:34:47 +0800

    NF: implement gen_dmask function

---
+++ dnnbrain/utils/tests/test_util.py
@@ -0,0 +1,62 @@
+import os
+import pytest
+
+from os.path import join as pjoin
+from dnnbrain.utils import util as db_util
+
+DNNBRAIN_TEST = pjoin(os.environ['DNNBRAIN_DATA'], 'test')
+TMP_DIR = pjoin(os.path.expanduser('~'), '.dnnbrain_tmp')
+if not os.path.isdir(TMP_DIR):
+    os.makedirs(TMP_DIR)
+
+
+def test_gen_dmask():
+
+    # prepare
+    layers1 = ['conv1']
+    layers2 = ['conv1', 'conv2']
+    channels1 = [1]
+    channels2 = [1, 2]
+    dmask_file = pjoin(DNNBRAIN_TEST, 'alexnet.dmask.csv')
+
+    # -assert parameters-
+    with pytest.raises(AssertionError):
+        db_util.gen_dmask()
+    with pytest.raises(AssertionError):
+        db_util.gen_dmask(layers1, dmask_file=dmask_file)
+    with pytest.raises(AssertionError):
+        db_util.gen_dmask(channels=channels1, dmask_file=dmask_file)
+
+    # -assert single layer-
+    # assert with channels
+    dmask = db_util.gen_dmask(layers1, channels2)
+    assert dmask.layers == layers1
+    assert dmask.get(layers1[0])['chn'] == channels2
+
+    # assert without channels
+    dmask = db_util.gen_dmask(layers1)
+    assert dmask.layers == layers1
+    assert dmask.get(layers1[0]).get('chn') is None
+
+    # -assert multi layers-
+    # assert with channels
+    dmask = db_util.gen_dmask(layers2, channels2)
+    assert dmask.layers == layers2
+    for layer, chn in zip(layers2, channels2):
+        assert dmask.get(layer)['chn'] == [chn]
+    with pytest.raises(Exception):
+        db_util.gen_dmask(layers2, channels1)
+
+    # assert without channels
+    dmask = db_util.gen_dmask(layers2)
+    assert dmask.layers == layers2
+    for layer in layers2:
+        assert dmask.get(layer).get('chn') is None
+
+    # -assert dmask file-
+    dmask = db_util.gen_dmask(dmask_file=dmask_file)
+    assert dmask.layers == ['conv5', 'fc3']
+    assert dmask.get('conv5')['chn'] == [1, 2, 3]
+    assert dmask.get('conv5')['row'] == [4, 5]
+    assert dmask.get('conv5')['col'] == [6, 7, 8]
+    assert dmask.get('fc3')['chn'] == [1, 2, 3]
