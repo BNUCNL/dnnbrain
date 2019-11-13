09e68e1cf9f02f21b3e7f5b728e8ee959aeafe33
sunshineDrizzle <954830460@qq.com>
2019-11-13 20:30:45 +0800

    RF: adapt dnn_fe to new design

--- bin/tests/test_dnn_fe.sh
+++ bin/tests/test_dnn_fe.sh
@@ -3,4 +3,4 @@
 TMP_DIR=~/.dnnbrain_tmp
 mkdir -p $TMP_DIR
 
-dnn_fe -act $DNNBRAIN_DATA/test/image/sub-CSI1_ses-01_imagenet.act.h5 -layer conv5 fc3 -meth pca 3 -axis chn -out $TMP_DIR/dnn_fe.act.h5
+dnn_fe -act $DNNBRAIN_DATA/test/image/sub-CSI1_ses-01_imagenet.act.h5 -meth pca 3 -axis chn -out $TMP_DIR/dnn_fe.act.h5
