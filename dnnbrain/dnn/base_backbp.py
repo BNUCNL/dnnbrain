1fcff16f6e317bcca47ebc4ff58348ae830136ca
zhenzonglei <zhenzonglei@gmail.com>
2019-11-14 21:18:27 -0500

    RF:Gradient backprop

--- dnnbrain/dnn/base_backbp.py
+++ dnnbrain/dnn/base_backbp.py
@@ -1,15 +1,6 @@
-import numpy as np
-import math
 import torch
 from torch.nn import ReLU
-from torch.optim import Adam
 from abc import ABC, abstractmethod
-try:
-    from misc_functions import preprocess_image, recreate_image
-except ModuleNotFoundError:
-    pass
-    #raise Exception('Please install misc_functions in your work station')
-    
 
 class BackPropGradient(ABC):
     """ 
@@ -18,7 +9,7 @@ class BackPropGradient(ABC):
     """
     def __init__(self, model):
         self.model = model
-        self.activation = None
+        self.activation = []
         self.gradient = None
         
     def set_layer(self, layer = None, channel = None):
@@ -27,9 +18,15 @@ class BackPropGradient(ABC):
     
     @abstractmethod
     def register_hooks(self):
-        return NotImplemented
+        """
+        Define regsister hook and register them to specific layer and channel.
+        As this a abstract method, it is needed to be override in every childclass
+        """
 
     def gradient(self, image):
+        """
+        Compute gradient with back propgation algorithm 
+        """
         self.register_hooks()
         # Forward
         self.model(image)
@@ -43,33 +40,37 @@ class BackPropGradient(ABC):
         return gradient
     
     def smooth_gradient(self, image): 
-        'Please do your implementation here'
+        """
+        Compute smoothed gradient. 
+        It will use the graident method to compute the gradient and then smooth it
+        """
         pass
+        
 
 class VanilaBackPropGradient(BackPropGradient):
     """ 
     A class to compute vanila Backprob gradient for a image.
     """
-    
     def register_hooks(self):
-        def forward_hook(module, input_feature,output_feature):
-            self.activation = output_feature[:, self.channel]
+        """
+        Overrie the abstract method from BackPropGradient class to 
+        define a specific hook for vanila backprop gradient. 
+        """  
+        def forward_hook(module, feat_in,feat_out):
+            self.activation.append(feat_out[:, self.channel])
         def backward_hook(module,grad_in,grad_out):
-            self.gradient = grad_out[0]
-            
+            self.gradient = grad_in[0]
+        
+        # register forward hook to the target layer
         module = self.model
-        for k in self.layer:
-            module = module._modules[k]
-            
+        for L in self.layer:
+            module = module._modules[L]
         module.register_forward_hook(forward_hook)
-
+        
+        # register backward to the first layer
         first_layer = self.model[0][1]
         first_layer.register_backward_hook(backward_hook)
 
-          
-
-        
-    
 class GuidedBackPropGradient(BackPropGradient):
     """ 
     A class to compute Guided Backprob gradient for a image.
@@ -77,36 +78,45 @@ class GuidedBackPropGradient(BackPropGradient):
     """    
     
     def register_hooks(self):
-        def register_first_layer_hook(module,grad_in,grad_out):
+        """
+        Overrie the abstract method from BackPropGradient class to 
+        define a specific hook for guided backprop gradient. 
+        """  
+        def first_layer_backward_hook(module,grad_in,grad_out):
             self.out_img = grad_in[0]
-        def forward_hook(module,input_feature,output_feature):
-            self.activation = output_feature[:, self.channel]
+        def targ_layer_forward_hook(module,feat_in,feat_out):
+            self.activation.append(feat_out[:, self.channel])
+        def forward_hook(module,feat_in,feat_out):
+            self.activation.append(feat_out)
+            # = feat_out[:, self.channel]
         def backward_hook(module,grad_in,grad_out):
-            grad = self.activation.pop()
-            grad[grad > 0] = 1
-            g_positive = torch.clamp(grad_out[0],min = 0.)
-            result_grad = grad * g_positive
-            return (result_grad,)
+            act = self.activation.pop()
+            act[act > 0] = 1
+            grad = torch.clamp(grad_out[0],min = 0.)
+            grad = grad * act
+            return (grad,)
         
+        # register hook for target module 
         targ_module = self.model
-        for k in self.layer:
-            targ_module = targ_module._modules[k]
-            
-            
+        for L in self.layer:
+            targ_module = targ_module._modules[L]
+        targ_module.register_forward_hook(targ_layer_forward_hook)
+        targ_module.register_backward_hook(backward_hook) 
+        
+        # register forward and backward hook to all layers bellow targe layer
         modules = list(self.model.features.named_children())
         for name,module in modules:
-            if module is targ_module:
-                module.register_forward_hook(forward_hook)
-                module.register_backward_hook(backward_hook)
+            if module is not targ_module:
+                if isinstance(module, ReLU):
+                    module.register_forward_hook(forward_hook)
+                    module.register_backward_hook(backward_hook)
+            else:         
                 break
 
-            if isinstance(module,nn.ReLU):
-                module.register_forward_hook(forward_hook)
-                module.register_backward_hook(backward_hook)
-        
-    
+
+        # register backward to the first layer
         first_layer = self.model[0][1]
-        first_layer.register_backward_hook(register_first_layer_hook)
+        first_layer.register_backward_hook(first_layer_backward_hook)
 
              
 
