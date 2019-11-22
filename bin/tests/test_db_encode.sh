3fe7cb2463cec86e8aed748ebe91e499d62f43fc
sunshineDrizzle <954830460@qq.com>
2019-11-22 16:43:55 +0800

    RF: adapt db_encode to new design

--- bin/tests/test_db_encode.sh
+++ bin/tests/test_db_encode.sh
@@ -4,14 +4,14 @@ TMP_DIR=~/.dnnbrain_tmp
 mkdir -p $TMP_DIR
 
 # do uva with .nii
-db_encode -anal uva -act $DNNBRAIN_DATA/test/image/sub-CSI1_ses-01_imagenet.act.h5 -dmask $DNNBRAIN_DATA/test/alexnet.dmask.csv -iteraxis channel -resp $DNNBRAIN_DATA/test/sub-CSI1_ses-01_imagenet_beta_L.nii.gz -bmask $DNNBRAIN_DATA/test/PHA1_L.nii.gz -model glm -out $TMP_DIR
+db_encode -anal uv -act $DNNBRAIN_DATA/test/image/sub-CSI1_ses-01_imagenet.act.h5 -dmask $DNNBRAIN_DATA/test/alexnet.dmask.csv -iteraxis channel -resp $DNNBRAIN_DATA/test/sub-CSI1_ses-01_imagenet_beta_L.nii.gz -bmask $DNNBRAIN_DATA/test/PHA1_L.nii.gz -model glm -out $TMP_DIR
 
 # do mva with .nii
-db_encode -anal mva -act $DNNBRAIN_DATA/test/image/sub-CSI1_ses-01_imagenet.act.h5 -dmask $DNNBRAIN_DATA/test/alexnet.dmask.csv -iteraxis channel -resp $DNNBRAIN_DATA/test/sub-CSI1_ses-01_imagenet_beta_L.nii.gz -bmask $DNNBRAIN_DATA/test/PHA1_L.nii.gz -model glm -out $TMP_DIR
+db_encode -anal mv -act $DNNBRAIN_DATA/test/image/sub-CSI1_ses-01_imagenet.act.h5 -dmask $DNNBRAIN_DATA/test/alexnet.dmask.csv -iteraxis channel -resp $DNNBRAIN_DATA/test/sub-CSI1_ses-01_imagenet_beta_L.nii.gz -bmask $DNNBRAIN_DATA/test/PHA1_L.nii.gz -model glm -out $TMP_DIR
 
 # do uva with .roi.h5
-db_encode -anal uva -act $DNNBRAIN_DATA/test/image/sub-CSI1_ses-01_imagenet.act.h5 -dmask $DNNBRAIN_DATA/test/alexnet.dmask.csv -iteraxis channel -resp $DNNBRAIN_DATA/test/PHA1.roi.h5 -roi PHA1_R -model glm -out $TMP_DIR
+db_encode -anal uv -act $DNNBRAIN_DATA/test/image/sub-CSI1_ses-01_imagenet.act.h5 -dmask $DNNBRAIN_DATA/test/alexnet.dmask.csv -iteraxis channel -resp $DNNBRAIN_DATA/test/PHA1.roi.h5 -roi PHA1_R -model glm -out $TMP_DIR
 
 # do mva with .roi.h5
-db_encode -anal mva -act $DNNBRAIN_DATA/test/image/sub-CSI1_ses-01_imagenet.act.h5 -dmask $DNNBRAIN_DATA/test/alexnet.dmask.csv -iteraxis channel -resp $DNNBRAIN_DATA/test/PHA1.roi.h5 -roi PHA1_R -model glm -out $TMP_DIR
+db_encode -anal mv -act $DNNBRAIN_DATA/test/image/sub-CSI1_ses-01_imagenet.act.h5 -dmask $DNNBRAIN_DATA/test/alexnet.dmask.csv -iteraxis channel -resp $DNNBRAIN_DATA/test/PHA1.roi.h5 -roi PHA1_R -model glm -out $TMP_DIR
 
