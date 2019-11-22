1baec53754e5d7dc85630467099603f44a08bb6d
sunshineDrizzle <954830460@qq.com>
2019-11-22 17:44:53 +0800

    NF: implement dnn_probe

---
+++ bin/tests/test_dnn_probe.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+TMP_DIR=~/.dnnbrain_tmp
+mkdir -p $TMP_DIR
+
+# probe fc3 to predict label (uv)
+dnn_probe -anal uv -act $DNNBRAIN_DATA/test/image/sub-CSI1_ses-01_imagenet.act.h5 -layer fc3 -stim $DNNBRAIN_DATA/test/image/sub-CSI1_ses-01_imagenet.stim.csv -beh label -model svc -cv 2 -out $TMP_DIR
+
+# probe fc3 to predict label (mv)
+dnn_probe -anal mv -act $DNNBRAIN_DATA/test/image/sub-CSI1_ses-01_imagenet.act.h5 -layer fc3 -stim $DNNBRAIN_DATA/test/image/sub-CSI1_ses-01_imagenet.stim.csv -beh label -model svc -cv 2 -out $TMP_DIR
+
