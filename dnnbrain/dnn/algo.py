20b7a2546c219c81c8e9d6465ac19a43a1c7465c
Gong_ZX <1045418215@qq.com>
2020-01-10 07:46:41 -0500

    Seperate all the Boolean judgement from interation

--- dnnbrain/dnn/algo.py
+++ dnnbrain/dnn/algo.py
@@ -261,7 +261,8 @@ class SynthesisImage(Algorithm):
     """
 
     def __init__(self, dnn, layer=None, channel=None,
-                 activ_metric='mean', regular_metric=None, precondition_metric=None,):
+                 activ_metric='mean', regular_metric=None, precondition_metric=None,
+                 save_out_interval=False,print_inter_loss=False,):
         """
         Parameters:
         ----------
@@ -273,7 +274,7 @@ class SynthesisImage(Algorithm):
         precondition_metric[str]: The metric method of precondition
         """
         super(SynthesisImage, self).__init__(dnn, layer, channel)
-        self.set_metric(activ_metric, regular_metric, precondition_metric)
+        self.set_metric(activ_metric, regular_metric, precondition_metric, save_out_interval,print_inter_loss)
         self.activ_loss = None
         self.optimal_image = None
 
@@ -282,10 +283,9 @@ class SynthesisImage(Algorithm):
         self.activ_losses = []
         self.regular_losses = []
 
-        #regularize parameter
-        self.GB_radius = 1
 
-    def set_metric(self, activ_metric, regular_metric,precondition_metric):
+    def set_metric(self, activ_metric, regular_metric,
+                   precondition_metric,save_out_interval,print_inter_loss):
         """
         Set metric methods
 
@@ -294,6 +294,7 @@ class SynthesisImage(Algorithm):
         activ_metric[str]: The metric method to summarize activation
         regular_metric[str]: The metric method of regularization
         precondition_metric[str]: The metric method of preconditioning
+        save_out_internal[str]: the method of saving the pics in interations
         """
         # activation metric setting
         if activ_metric == 'max':
@@ -323,6 +324,36 @@ class SynthesisImage(Algorithm):
         else:
             raise AssertionError('Only Gaussian Blur is supported!')
 
+        # saving interval pics in iteration setting
+        if save_out_interval is True:
+            self.save_out_interval = self._save_out
+        elif save_out_interval is False:
+            self.save_out_interval = self._close_save
+
+        # print interation loss
+        if print_inter_loss is True:
+            self.print_inter_loss = self._print_loss
+        elif print_inter_loss is False:
+            self.print_inter_loss = self._print_close
+
+    def _print_loss(self,i,step,n_iter,loss):
+        if i % step == 0:
+            print(f'Interation: {i}/{n_iter}; Loss: {loss}')
+
+    def _print_close(self,i,step,n_iter,loss):
+        pass
+
+
+    def _save_out(self,currti,save_interval,save_path):
+        if (currti+1) % save_interval == 0:
+            img_out = self.optimal_image[0].detach().numpy().copy()
+            img_out = ip.to_pil(img_out, True)
+            img_out.save(pjoin(save_path, f'synthesized_image_iter{currti+1}.jpg'))
+
+    def _close_save(self, currti,save_interval, save_path):
+        pass
+
+
     def _regular_default(self):
         reg = 0
         return reg
@@ -347,14 +378,14 @@ class SynthesisImage(Algorithm):
         self.regular_losses.append(reg.item())
         return reg
 
-    def _precondition_default(self):
+    def _precondition_default(self, GB_radius):
         precond_image = self.optimal_image[0].detach().numpy()
         precond_image = ip.to_tensor(precond_image).float()
         precond_image = copy.deepcopy(precond_image)
         return precond_image
 
-    def _gaussian_blur(self):
-        precond_image = filters.gaussian(self.optimal_image[0].detach().numpy(), self.GB_radius)
+    def _gaussian_blur(self, radius):
+        precond_image = filters.gaussian(self.optimal_image[0].detach().numpy(), radius)
         precond_image = ip.to_tensor(precond_image).float()
         precond_image = copy.deepcopy(precond_image)
         return precond_image
@@ -382,7 +413,7 @@ class SynthesisImage(Algorithm):
         return handle
 
     def synthesize(self, init_image=None, lr=0.1, regular_lambda=1, n_iter=30,
-                   save_path=None, save_interval=None):
+                   save_path=None, save_interval=None, GB_radius=None, step=1):
         """
         Synthesize the image which maximally activates target layer and channel
 
@@ -399,7 +430,7 @@ class SynthesisImage(Algorithm):
             If is None, do nothing.
             else, save_path must not be None.
                 Save out synthesized images per 'save interval' iterations.
-
+        GB_radius[float]
         Return:
         ------
             [ndarray]: the synthesized image with shape as (n_chn, height, width)
@@ -428,17 +459,14 @@ class SynthesisImage(Algorithm):
             optimizer = Adam([self.optimal_image], lr=lr)
 
             # save out
-            # if save_interval is not None and i % save_interval == 0:
-            #     img_out = self.optimal_image[0].detach().numpy().copy()
-            #     img_out = ip.to_pil(img_out, True)
-            #     img_out.save(pjoin(save_path, f'synthesized_image_iter{i}.jpg'))
+            self.save_out_interval(i,save_interval,save_path)
+
 
             # Forward pass layer by layer until the target layer
             # to triger the hook funciton.
             self.dnn.model(self.optimal_image)
 
             # computer loss
-
             loss = self.activ_loss + regular_lambda * self.regular_metric()
 
             # zero gradients
@@ -448,11 +476,11 @@ class SynthesisImage(Algorithm):
             loss.backward()
             # Update image
             optimizer.step()
-            if i % 149 == 0:
-                print(f'Iteration: {i}/{n_iter}; Loss: {loss.item()}')
 
+            # Print interation
+            self.print_inter_loss(i,step,n_iter,loss)
             # precondition
-            init_image = self.precondition_metric()
+            init_image = self.precondition_metric(GB_radius)
 
         # remove hook
         handle.remove()
