5b2e7ad67f310a1d56f49c0da05039f9e2acd6f7
sunshineDrizzle <954830460@qq.com>
2020-02-12 13:27:37 +0800

    EHN: allow imgarray_show to display image names

--- dnnbrain/utils/plot.py
+++ dnnbrain/utils/plot.py
@@ -3,7 +3,7 @@ import numpy as np
 
 
 def imgarray_show(x, nrows=1, ncols=1, row_label=None, vmin=None, vmax=None,
-                  figsize=(10, 6), cmap='coolwarm', frame_on=True,
+                  figsize=(10, 6), cmap='coolwarm', frame_on=True, img_names=None,
                   show=True, save_path=None):
     """
     create a figure showing multiple images.
@@ -20,6 +20,7 @@ def imgarray_show(x, nrows=1, ncols=1, row_label=None, vmin=None, vmax=None,
     cmap[str]: The Colormap instance or registered colormap name used 
         to map scalar data to colors. 
     frame_on[bool]: set whether the axes rectangle patch is drawn
+    img_names[list]: image names with the same length as x
     show[bool]: set whether the figure is displayed
     save_path[str]: file path to save the figure
     """
@@ -30,8 +31,10 @@ def imgarray_show(x, nrows=1, ncols=1, row_label=None, vmin=None, vmax=None,
                             figsize=figsize)
 
     for i, ax in enumerate(axs.flat[:len(x)]):
-        if np.mod(i, ncols) == 0:
+        if row_label is not None and np.mod(i, ncols) == 0:
             ax.set_ylabel(row_label[i//ncols])
+        if img_names is not None:
+            ax.set_xlabel(img_names[i])
         ax.imshow(x[i], cmap=cmap, vmin=vmin, vmax=vmax)
     plt.tight_layout()
     
