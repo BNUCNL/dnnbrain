2583b4a2d88d28964bf26bb51138f8ee1692ba72
Blink <1458940886@qq.com>
2020-04-11 15:49:24 +0800

    update minstim and rf in cmd and add translate metric in base

--- bin/tests/test_dnn_rf_us.sh
+++ bin/tests/test_dnn_rf_us.sh
@@ -4,5 +4,5 @@ TMP_DIR=~/.dnnbrain_tmp
 mkdir -p $TMP_DIR
 
 # test
-dnn_rf_us -net AlexNet -stim $DNNBRAIN_DATA/test/image/sub-CSI1_ses-01_imagenet.stim.csv -layer conv5 -channel 1 -ip_metric bicubic -up_thres 0.68 -out $TMP_DIR/rf_us
+dnn_rf_us -net AlexNet -stim /$DNNBRAIN_DATA/test/image/sub-CSI1_ses-01_imagenet.stim.csv -layer conv5 -channel 122 -ip_metric bicubic -up_thres 0.68 -out $TMP_DIR/rf_us
 
--- dnnbrain/dnn/tests/test_base.py
+++ dnnbrain/dnn/tests/test_base.py
@@ -368,6 +368,29 @@ class TestImageProcessor:
         assert isinstance(pil1, Image.Image)
         np.testing.assert_equal(pil1, pil2)
 
+    def test_translate(self):
+
+        #prepare image
+        img = np.random.normal(size=(3,50,50))
+        bkg = np.zeros((3,224,224))
+        
+        #prepare parameters
+        startpoint = (20,20)
+        endpoint = (20,50)
+        stride = 2
+        
+        img_tran = self.ip.translate(img, bkg, startpoint, endpoint, stride)
+        
+        # assert shape
+        num = int((endpoint[1]-startpoint[1])/stride)+1
+        assert img_tran.shape[0] == num
+        assert (img_tran.shape[2],img_tran.shape[3]) == (bkg.shape[1], bkg.shape[2])
+        
+        # assert pixel
+        pic = img_tran[15]
+        pixel = pic[0,30,40]
+        assert pixel == 0
+    
     def test_norm(self):
 
         # assert L1
