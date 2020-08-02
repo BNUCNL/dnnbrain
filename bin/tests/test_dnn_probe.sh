74e108d2bffb676f5b6e71a8a1f04b0f69b67e95
sunshineDrizzle <954830460@qq.com>
2020-08-02 20:29:16 +0800

    RF: adapt dnn_probe to new UV and MV mapping

--- bin/tests/test_dnn_probe.sh
+++ bin/tests/test_dnn_probe.sh
@@ -4,7 +4,7 @@ TMP_DIR=~/.dnnbrain_tmp
 mkdir -p $TMP_DIR
 
 # probe fc3 to predict label (uv)
-dnn_probe -anal uv -act $DNNBRAIN_DATA/test/image/sub-CSI1_ses-01_imagenet.act.h5 -layer fc3 -stim $DNNBRAIN_DATA/test/image/sub-CSI1_ses-01_imagenet.stim.csv -beh label response -model svc -cv 2 -out $TMP_DIR
+dnn_probe -anal uv -act $DNNBRAIN_DATA/test/image/sub-CSI1_ses-01_imagenet.act.h5 -layer fc3 -stim $DNNBRAIN_DATA/test/image/sub-CSI1_ses-01_imagenet.stim.csv -beh label -model svc -cv 2 -out $TMP_DIR
 
 # probe fc3 to predict RT (mv)
 dnn_probe -anal mv -act $DNNBRAIN_DATA/test/image/sub-CSI1_ses-01_imagenet.act.h5 -layer fc3 -stim $DNNBRAIN_DATA/test/image/sub-CSI1_ses-01_imagenet.stim.csv -beh RT -model glm -cv 2 -out $TMP_DIR
