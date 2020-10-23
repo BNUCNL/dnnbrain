b3367e7939971fb21c1ad8794dcd60d8eab11b5c
sunshineDrizzle <954830460@qq.com>
2020-10-23 21:22:58 +0800

    NF: preprocess and postprocess for VGGish

--- dnnbrain/dnn/models.py
+++ dnnbrain/dnn/models.py
@@ -13,6 +13,7 @@ from torchvision import transforms
 from torchvision import models as tv_models
 from dnnbrain.dnn.core import Stimulus, Activation
 from dnnbrain.dnn.base import ImageSet, VideoSet, dnn_mask, array_statistic, VideoClipSet
+from dnnbrain.utils import vggish_input, vggish_params
 
 DNNBRAIN_MODEL = pjoin(os.environ['DNNBRAIN_DATA'], 'models')
 
@@ -155,17 +156,123 @@ class VGGishModel(nn.Module):
 
 class VGGish:
 
-    def __init__(self, pretrained=True):
+    def __init__(self, pretrained=True, preprocess=True, postprocess=True):
+
         self.model = VGGishModel()
         if pretrained:
             self.model.load_state_dict(torch.load(
                 pjoin(DNNBRAIN_MODEL, 'vggish.pth')))
 
-    def _preprocess(self, x, fs):
-        pass
+        self.preprocess = preprocess
+        self.postprocess = postprocess
+        if self.postprocess:
+            self.postproc = self._postprocess()
+            if pretrained:
+                state_dict = torch.load(pjoin(DNNBRAIN_MODEL, 'vggish_pca.pth'))
+                # TODO: Convert the state_dict to torch
+                state_dict[vggish_params.PCA_EIGEN_VECTORS_NAME] = torch.as_tensor(
+                    state_dict[vggish_params.PCA_EIGEN_VECTORS_NAME], dtype=torch.float
+                )
+                state_dict[vggish_params.PCA_MEANS_NAME] = torch.as_tensor(
+                    state_dict[vggish_params.PCA_MEANS_NAME].reshape(-1, 1), dtype=torch.float
+                )
+
+                self.postproc.load_state_dict(state_dict)
+
+    def __call__(self, x, fs=None):
+        if self.preprocess:
+            x = self._preprocess(x, fs)
+        x = self.model(x)
+        if self.postprocess:
+            x = self.postproc(x)
+        return x
+
+    def _preprocess(self, x, fs=None):
+        if isinstance(x, np.ndarray):
+            x = vggish_input.waveform_to_examples(x, fs)
+        elif isinstance(x, str):
+            x = vggish_input.wavfile_to_examples(x)
+        else:
+            raise AttributeError
+        return x
 
     def _postprocess(self):
-        pass
+        class Postprocessor(nn.Module):
+            """Post-processes VGGish embeddings. Returns a torch.Tensor instead of a
+            numpy array in order to preserve the gradient.
+
+            "The initial release of AudioSet included 128-D VGGish embeddings for each
+            segment of AudioSet. These released embeddings were produced by applying
+            a PCA transformation (technically, a whitening transform is included as well)
+            and 8-bit quantization to the raw embedding output from VGGish, in order to
+            stay compatible with the YouTube-8M project which provides visual embeddings
+            in the same format for a large set of YouTube videos. This class implements
+            the same PCA (with whitening) and quantization transformations."
+            """
+
+            def __init__(self):
+                """Constructs a postprocessor."""
+                super(Postprocessor, self).__init__()
+                # Create empty matrix, for user's state_dict to load
+                self.pca_eigen_vectors = torch.empty(
+                    (vggish_params.EMBEDDING_SIZE, vggish_params.EMBEDDING_SIZE,),
+                    dtype=torch.float,
+                )
+                self.pca_means = torch.empty(
+                    (vggish_params.EMBEDDING_SIZE, 1), dtype=torch.float
+                )
+
+                self.pca_eigen_vectors = nn.Parameter(self.pca_eigen_vectors, requires_grad=False)
+                self.pca_means = nn.Parameter(self.pca_means, requires_grad=False)
+
+            def postprocess(self, embeddings_batch):
+                """Applies tensor postprocessing to a batch of embeddings.
+
+                Parameters
+                ----------
+                embeddings_batch : PyTorch tensor
+                    shape=(batch_size, embedding_size)
+                    containing output from the embedding layer of VGGish.
+
+                Returns
+                -------
+                A tensor of the same shape as the input, containing the PCA-transformed,
+                quantized, and clipped version of the input.
+                """
+                assert len(embeddings_batch.shape) == 2, "Expected 2-d batch, got %r" % (
+                    embeddings_batch.shape,
+                )
+                assert (
+                        embeddings_batch.shape[1] == vggish_params.EMBEDDING_SIZE
+                ), "Bad batch shape: %r" % (embeddings_batch.shape,)
+
+                # Apply PCA.
+                # - Embeddings come in as [batch_size, embedding_size].
+                # - Transpose to [embedding_size, batch_size].
+                # - Subtract pca_means column vector from each column.
+                # - Premultiply by PCA matrix of shape [output_dims, input_dims]
+                #   where both are are equal to embedding_size in our case.
+                # - Transpose result back to [batch_size, embedding_size].
+                pca_applied = torch.mm(self.pca_eigen_vectors, (embeddings_batch.t() - self.pca_means)).t()
+
+                # Quantize by:
+                # - clipping to [min, max] range
+                clipped_embeddings = torch.clamp(
+                    pca_applied, vggish_params.QUANTIZE_MIN_VAL, vggish_params.QUANTIZE_MAX_VAL
+                )
+                # - convert to 8-bit in range [0.0, 255.0]
+                quantized_embeddings = torch.round(
+                    (clipped_embeddings - vggish_params.QUANTIZE_MIN_VAL)
+                    * (
+                            255.0
+                            / (vggish_params.QUANTIZE_MAX_VAL - vggish_params.QUANTIZE_MIN_VAL)
+                    )
+                )
+                return torch.squeeze(quantized_embeddings)
+
+            def forward(self, x):
+                return self.postprocess(x)
+        return Postprocessor()
 
 
 class DNN:
